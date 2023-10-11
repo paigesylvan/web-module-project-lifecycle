b20fbd390c24e6752a3fea2bb15052d5f64ee67a
PaigeSylvan <paigesylvan@gmail.com>
2023-10-10 22:26:23 -0500

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './Form'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -82,11 +83,13 @@ export default class App extends React.Component {
         </div>
 
         <div>
-          <form id="todoForm" onSubmit={this.onTodoFormSubmit} >
-            <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
-            <input type="submit"></input>
-            <button onClick={this.toggleDisplayCompleteds}>{this.state.displayCompleteds ? 'Hide' : 'Show'}Clear Completed</button>
-          </form>
+          <Form 
+            onTodoFormSubmit ={ this.onTodoFormSubmit}
+            todoNameInput={this.state.todoNameInput}
+            onTodoNameInputChange={this.onTodoNameInputChange}
+            toggleDisplayCompleteds={this.toggleDisplayCompleteds}
+            displayCompleteds={this.displayCompleteds}
+          />
         </div>
 
       </div>
